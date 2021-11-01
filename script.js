02804e1a5f6d15c02f1520411a83646f0d8a0015
leeyoounjung <isjj9905@gmail.com>
2021-11-01 16:02:50 +0900

    IBK배구단 코딩 8일차 완성

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ $(document).ready(function(){
                    { breakpoint: 320, // 화면의 넓이가 320px 이상일 때 
                     settings: { slidesToShow: 1, slidesToScroll: 1 } } ]
   
-      
     });
     
+    $('.altostv-wrap > .altostv-bottom-box > .slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow: $(".altostv-wrap > .btn > .left-btn"),
+      nextArrow: $(".altostv-wrap > .btn > .right-btn"),
+    });
+      
+      $('.photo-wrap > .photo-bottom-box > .slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow: $(".photo-wrap > .btn > .left-btn"),
+      nextArrow: $(".photo-wrap > .btn > .right-btn"),
+    });
+
   });
